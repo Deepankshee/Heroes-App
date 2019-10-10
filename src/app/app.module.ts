100a64336257009c2a010cfeabce6b9e6f06ef40
Deepankshee <jain.deepankshee190@gmail.com>
2019-10-10 18:06:34 +0530

    in progress services in hero app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
+
 
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    FormsModule,
     RouterModule.forRoot([
       { path: '', component: AppComponent },
     ])
   ],
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [ AppComponent ],
+  providers: [HeroService, MessageService]
 })
 export class AppModule { }
 
